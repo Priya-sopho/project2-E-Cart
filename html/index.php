95fb6220af2c385767b74f4487fd05696bb45a6e
Priya <prrani28@gmail.com>
2017-03-12 11:48:49 +0530

    Successfully connected database

--- html/index.php
+++ html/index.php
@@ -1,4 +1,17 @@
 <?php 
   require("../includes/config.php");
   echo "Hello";
+  //To check connection
+
+  $row = mysql_query('Select * from College');
+  if(!$row)
+  echo "Error!";
+  else
+  {
+   while($r=mysql_fetch_assoc($row))
+  {
+    echo '<br>'.$r["Cid"].' ';
+    echo $r["Name"];
+  }
+ }   
 ?>  
